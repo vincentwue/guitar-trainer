ae65f10314fc92805cc9c0e4dcb0c0aaa7cc9e83
vincentwue <vini96@web.com>
2021-05-04 17:11:59 +0200

    colored

--- src/components/Pattern.tsx
+++ src/components/Pattern.tsx
@@ -6,6 +6,7 @@ import { hexToRgb } from '../utils/utils';
 import Cell, { DisplayType } from "./Cell"
 import classNames from 'classnames';
 import { useMasterStore } from '../utils/store';
+import { notesArray } from '../definitions/notes';
 
 type PatternProps = {
   first: RenderablePattern,
@@ -25,32 +26,50 @@ export default function Pattern(props: PatternProps) {
   const a = []
   const masterState = useMasterStore()
 
-
+  let blackCount = 0
   for (let index = 0; index < 50; index++) {
 
     const noBlack = index % 7 === 0 || index % 7 === 3
+    if (!noBlack) blackCount++
 
-    const stepsFromC = index % 7
+    const stepsFromC = (blackCount+index) % 12
 
     const inScale = first.notes.find(note => note.intervalFromC.step === stepsFromC)
     const isRoot = first.notes[0].intervalFromC.step === stepsFromC
+    const inScale2 = second.notes.find(note => note.intervalFromC.step === stepsFromC)
+    const isRoot2 = second.notes[0].intervalFromC.step === stepsFromC
+    
+    const stepsFromCBlack = (blackCount+index-1) % 12
+    
+    const isBlackinScale = first.notes.find(note => note.intervalFromC.step === stepsFromCBlack)
+    const isBlackRoot = first.notes[0].intervalFromC.step === stepsFromCBlack
+    const isBlackinScale2 = second.notes.find(note => note.intervalFromC.step === stepsFromCBlack)
+    const isBlackRoot2 = second.notes[0].intervalFromC.step === stepsFromCBlack
 
+    const rootColor="red"
 
     const whiteKey = <div className={classes.whiteKey}
       style={{
-        backgroundColor: isRoot ? "yellow" : inScale ? "green" : "white"
+        backgroundColor: isRoot ? rootColor : inScale ? "green" : "white"
       }}
     >
-      
 
       {!noBlack &&
         <div className={classes.blackKey}
-          style={{
-            left: -6,
-            width: 10
-          }}
-        ></div>
+        style={{
+          left: -6,
+          width: 6,
+          border:"2px solid black",
+          backgroundColor:isBlackRoot ? rootColor : isBlackinScale ? "green" : "white"
+        }}
+        >
+
+        </div>
       }
+      <div className={classes.whiteKeyNoteName}>
+      {notesArray[stepsFromC].id}
+
+      </div>
 
     </div>
     a.push(whiteKey)
