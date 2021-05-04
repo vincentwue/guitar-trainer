35805636be9b7e4927b1485a962a13daa4d8c23b
vincentwue <vini96@web.com>
2021-05-04 16:47:31 +0200

    init

--- src/components/Pattern.tsx
+++ src/components/Pattern.tsx
@@ -1,9 +1,8 @@
-import classes from './Patterns.module.css';
+import classes from './Pattern.piano.module.css';
 import React, { useEffect } from 'react';
 import { RenderablePattern } from '../definitions/renderables';
 // import { RenderableArray, RenderableState } from '../definitions/renderables';
 import { hexToRgb } from '../utils/utils';
-// import classes from "./Pattern.module.css"
 import Cell, { DisplayType } from "./Cell"
 import classNames from 'classnames';
 import { useMasterStore } from '../utils/store';
@@ -12,96 +11,63 @@ type PatternProps = {
   first: RenderablePattern,
   second: RenderablePattern,
   secondHidden: boolean,
-  n:number,
+  n: number,
   /*   firstIntervals: boolean,
     secondIntervals: boolean, */
 }
 
 export default function Pattern(props: PatternProps) {
 
-  console.log(props)
+  console.log("pattern", props)
 
-  const reversedSecondStrings = props.second.strings
+  const { second, first } = props
+
+  const a = []
   const masterState = useMasterStore()
 
 
+  for (let index = 0; index < 50; index++) {
 
-  const mappedNotes = props.first.strings.map((string, stringIndex) => {
+    const noBlack = index % 7 === 0 || index % 7 === 3
 
-    const mappedString = string.map((renderableNote, fretIndex) => {
-      // const color = hexToRgb(renderableNote.interval.standardColor)
+    const stepsFromC = index % 7
 
-      /*       let borderRight = "4px solid grey";
-            let borderLeft = "4px solid grey";
-            if (fretIndex === 0) borderRight = "5px solid black"
-            if (fretIndex === 11) borderRight = "5px solid black"
-            if (fretIndex === 12) borderRight = "5px solid black"
-            if (fretIndex === 23) borderRight = "5px solid black"
-            if (fretIndex === 1) borderLeft = "5px solid black"
-            if (fretIndex === 13) borderLeft = "5px solid black"
-            if (fretIndex === 12) borderLeft = "5px solid black"
-            if (fretIndex === 24) borderLeft = "5px solid black" */
+    const inScale = first.notes.find(note => note.intervalFromC.step === stepsFromC)
+    const isRoot = first.notes[0].intervalFromC.step === stepsFromC
 
-      return <div key={fretIndex} className={classNames(classes["fret" + fretIndex], classes.fret)} style={{
-        gridColumnStart: fretIndex + 1,
-        gridColumnEnd: fretIndex + 2,
-        gridRowStart: props.first.strings.length - stringIndex,
-        gridRowEnd: props.first.strings.length - stringIndex,
-        // backgroundColor: color,
-        // minWidth:40,
-        // }}>{note.hidden ? "" : props.firstIntervals ? note.interval.standardSymbol : note.note.id}</div>
-      }}><Cell
-        secondHidden={props.secondHidden}
-        first={{ renderableNote, colors: DisplayType.Intervals, symbols: DisplayType.Notes }}
-        second={{ renderableNote: reversedSecondStrings[stringIndex][fretIndex], colors: DisplayType.Intervals, symbols: DisplayType.Notes }}
-      ></Cell></div>
-      // if (note.hidden) return <div className={classes.note + " " + classes.hidden}></div>
 
-      // else return <div style={{ backgroundColor: color }} className={classes.text + " " + classes.note}>{props.firstIntervals ? note.interval.standardSymbol : note.note.id}</div>
-    })
+    const whiteKey = <div className={classes.whiteKey}
+      style={{
+        backgroundColor: isRoot ? "yellow" : inScale ? "green" : "white"
+      }}
+    >
+      
 
-    return mappedString
-  })
-
-  const mappedFretNumbers = props.first.fretsArray.map((n, i) => {
-    return <div key={i} className={classes.preFretNumbers} style={{
-      gridColumnStart: i + 1,
-      gridColumnEnd: i + 2,
-      gridRowStart: props.first.strings.length + 2,
-      gridRowEnd: props.first.strings.length + 3,
-      // textAlign:"center",
-      // minWidth:40,
-    }}>{n ? n : ""}</div>
-  })
-  const mappedFretNumbers2 = props.first.fretsArray.map((n, i) => {
-    return <div key={i} className={classes.postFretNumbers} style={{
-      gridColumnStart: i + 1,
-      gridColumnEnd: i + 2,
-      gridRowStart: 0,
-      gridRowEnd: 1,
-      // textAlign:"center",
-      // minWidth:40,
-    }}>{n ? n : ""}</div>
-  })
-
-  return (
-    <div >
-      <div className={classNames(classes.heading, !masterState.hideLegends ? classes.hidden : "")}>
-
-        {props.n + " - " + props.first.id + " - " + props.second.id}
-
-      </div>
-      <div className={classes.grid} style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(" + props.first.fretsArray.length + ", 1fr)",
-        gridTemplateRows: "repeat(" + props.first.strings.length + ", 1fr)",
-      }}>
-        {mappedNotes}
-        {mappedFretNumbers2}
-        {!masterState.hideLegends && mappedFretNumbers}
-      </div>
+      {!noBlack &&
+        <div className={classes.blackKey}
+          style={{
+            left: -6,
+            width: 10
+          }}
+        ></div>
+      }
 
     </div>
+    a.push(whiteKey)
+
+  }
+
+
+  return (<div>
+    <div className={classNames(classes.heading, !masterState.hideLegends ? classes.hidden : "")}>
+
+      {props.n + " - " + props.first.id + " - " + props.second.id}
+
+    </div>
+    <div className={classes.wrapper}>
+      {a}
+    </div>
+  </div>
   );
 }
 
